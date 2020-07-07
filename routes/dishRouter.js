9c7a1abb00b8b16884ba73b2609c98c5ade7515a
Pavlo Marii <pavlomarii@gmail.com>
2020-07-07 14:21:27 +0300

    Assignment 2: Leaders and Promotions Endpoints

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -22,7 +22,6 @@ dishRouter.route('/')
 .post((req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
-        //console.log('Dish Created ', dish);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(dish);
