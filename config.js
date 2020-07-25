6f57a393ce3045ec6d1e740c7dd9a51658c53c3e
Pavlo <pavlomarii@gmail.com>
2020-07-25 18:48:06 +0300

    Play with gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientId: '290330405722790',
-        clientSecret: '4143f761435119dd6a3680308e178fd6'
-    }
-}
